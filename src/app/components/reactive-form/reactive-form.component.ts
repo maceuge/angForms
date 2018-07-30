6e03daea58d1c737448f1e4ba4babc0d59d57558
maceuge <maceuge@icloud.com>
2018-07-30 18:06:37 -0300

    Estructura de reactive form

--- src/app/components/reactive-form/reactive-form.component.ts
+++ src/app/components/reactive-form/reactive-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-reactive-form',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveFormComponent implements OnInit {
 
-  constructor() { }
+  forma: FormGroup;
+  pattern = "[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$";
+
+  constructor() {
+
+    this.forma = new FormGroup({
+      'nombre': new FormControl('', [Validators.required, Validators.minLength(5)]),
+      'apellido': new FormControl('', Validators.required),
+      'email': new FormControl('', [ Validators.required, Validators.pattern(this.pattern) ]),
+      'pass': new FormControl(''),
+    });
+
+   }
 
   ngOnInit() {
+  }
+
+  guardarCambios () {
+    console.log(this.forma.value);
+    console.log('FormGroup', this.forma);
+    this.forma.reset();
   }
 
 }
